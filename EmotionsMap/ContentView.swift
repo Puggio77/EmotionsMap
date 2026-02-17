b3521df5c4709b6a022fb85d6096600c15a3ab91
Puggio77 <147837226+Puggio77@users.noreply.github.com>
2026-02-17 12:08:48 +0100

    first commit

---
+++ EmotionsMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmotionsMap
+//
+//  Created by Riccardo Puggioni on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmotionsMap/EmotionsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionsMapApp.swift
+//  EmotionsMap
+//
+//  Created by Riccardo Puggioni on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct EmotionsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
